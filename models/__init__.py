6116a099b8fef911e9adccd562066c95c68fae2d
Kaihua Tang <tkhchipaomian@gmail.com>
2024-09-18 16:31:26 +0800

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -5,7 +5,5 @@ from models.base_mp import BaseParallelLayer
 from models.llama.llama_mp import LlamaForCausalLM as LlamaForCausalLM_MP
 from models.llama.llama_tokenizer import LlamaTokenizer
 
-# qwen-vl
-#from models.qwenvl.qwenvl_mp import QWenLMHeadModel as QwenForCausalLM_MP
-#from models.qwenvl.qwenvl_config import QWenConfig
-#from models.qwenvl.qwenvl_tokenizer import QWenTokenizer
+# qwen
+from models.qwen.qwen_mp import QWenLMHeadModel as QwenForCausalLM_MP
